0f2407d31fb775b3bcdcc0a4ee0376ab90e285bc
tira_menali <77180304+soulmedicinas@users.noreply.github.com>
2025-04-01 13:05:28 -0700

    add: check if no date is provided

--- index.js
+++ index.js
@@ -28,8 +28,13 @@ app.get("/api/:date?", (req, res) => {
   let date;
   const inputDate = req.params.date;
 
-  
+  // Check if no date is provided (return current time)
+  if (!inputDate) {
+    date = new Date();
+  } 
 
+
+  
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
